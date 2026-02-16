8858bb634cc74f35360f9dce0dd6b8a4174b553f
prog <thanksforall2154@gmail.com>
2026-02-16 12:33:11 +0300

    fixing TabBar

--- lib/features/dashboard/presentation_layer/screens/dashboard.dart
+++ lib/features/dashboard/presentation_layer/screens/dashboard.dart
@@ -33,92 +33,209 @@ class DashboardScreen extends StatelessWidget {
         child: BlocBuilder<DashboardCubit, DashboardInitial>(
           builder: (context, state) {
             context.read<OrdersCubit>().refreshOrders();
-            return Scaffold(
-              backgroundColor: AppColors.white,
-              resizeToAvoidBottomInset: true,
-              extendBody: true,
-              bottomNavigationBar: CustomBottomNavBar(
-                currentIndex: state.currentIndex,
-                changeIndex: (index) {
-                  context.read<DashboardCubit>().changeIndex(index);
-                },
-              ),
-              appBar: AppBar(
+            return DefaultTabController(
+              length: state.currentIndex == 0 ? 6 : 0,
+              child: Scaffold(
                 backgroundColor: AppColors.white,
-                surfaceTintColor: AppColors.white,
-                title: state.currentIndex == 0
-                    ? Text('الطلبات')
-                    : state.currentIndex == 1
-                    ? Text('العروض')
-                    : state.currentIndex == 3
-                    ? Text('المنتجات')
-                    : state.currentIndex == 4
-                    ? Text('التعاملات')
-                    : Row(
-                        children: [
-                          CircleAvatar(child: Image.asset(AppImages.logo)),
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.02,
-                          ),
-                          Text(
-                            (context.read<AuthCubit>().state
-                                    as VerifyOtpSuccess)
-                                .user
-                                .name,
-                          ),
-                        ],
-                      ),
-                automaticallyImplyLeading: false,
-                actions: [
-                  state.currentIndex == 3
-                      ? ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: AppColors.primary,
-                            foregroundColor: AppColors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
+                resizeToAvoidBottomInset: true,
+                extendBody: true,
+                bottomNavigationBar: CustomBottomNavBar(
+                  currentIndex: state.currentIndex,
+                  changeIndex: (index) {
+                    context.read<DashboardCubit>().changeIndex(index);
+                  },
+                ),
+                appBar: AppBar(
+                  backgroundColor: AppColors.white,
+                  surfaceTintColor: AppColors.white,
+                  title: state.currentIndex == 0
+                      ? Text('الطلبات')
+                      : state.currentIndex == 1
+                      ? Text('العروض')
+                      : state.currentIndex == 3
+                      ? Text('المنتجات')
+                      : state.currentIndex == 4
+                      ? Text('التعاملات')
+                      : Row(
+                          children: [
+                            CircleAvatar(child: Image.asset(AppImages.logo)),
+                            SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.02,
                             ),
-                          ),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) => BlocProvider.value(
-                                  value: context.read<ProductsListCubit>(),
-                                  child: const AddProductScreen(),
-                                ),
+                            Text(
+                              (context.read<AuthCubit>().state
+                                      as VerifyOtpSuccess)
+                                  .user
+                                  .name,
+                            ),
+                          ],
+                        ),
+                  automaticallyImplyLeading: false,
+                  actions: [
+                    state.currentIndex == 3
+                        ? ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: AppColors.primary,
+                              foregroundColor: AppColors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
                               ),
-                            );
-                          },
-                          child: const Text(
-                            'إضافة منتج',
-                            style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
                             ),
-                          ),
-                        )
-                      : const SizedBox(),
-                  Builder(
-                    builder: (context) => IconButton(
-                      icon: const Icon(Icons.menu),
-                      onPressed: () {
-                        Scaffold.of(context).openDrawer();
-                      },
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => BlocProvider.value(
+                                    value: context.read<ProductsListCubit>(),
+                                    child: const AddProductScreen(),
+                                  ),
+                                ),
+                              );
+                            },
+                            child: const Text(
+                              'إضافة منتج',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
+                        : const SizedBox(),
+                    Builder(
+                      builder: (context) => IconButton(
+                        icon: const Icon(Icons.menu),
+                        onPressed: () {
+                          Scaffold.of(context).openDrawer();
+                        },
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              drawer: const SideMenu(),
-              body: IndexedStack(
-                index: state.currentIndex,
-                children: [
-                  const OrdersScreen(),
-                  const Center(child: Text('الرئيسية')),
-                  const OrdersMapScreen(),
-                  const ProductsScreen(),
-                  const CommissionScreen(),
-                ],
+                  ],
+                  bottom: state.currentIndex == 0
+                      ? TabBar(
+                          tabAlignment: TabAlignment.start,
+                          indicatorColor: AppColors.primary,
+                          isScrollable: true,
+                          labelColor: AppColors.primary,
+                          unselectedLabelColor: AppColors.textSecondary,
+                          dividerColor: Colors.transparent,
+                          tabs: [
+                            Tab(text: 'الكل'),
+                            Tab(
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.brightness_1,
+                                    color: AppColors.primary,
+                                    size:
+                                        (MediaQuery.of(context).size.height *
+                                            .808799) *
+                                        0.015,
+                                  ),
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width *
+                                        0.01,
+                                  ),
+                                  Text("جديدة"),
+                                ],
+                              ),
+                            ),
+                            Tab(
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.brightness_1,
+                                    color: AppColors.orderStatePending,
+                                    size:
+                                        (MediaQuery.of(context).size.height *
+                                            .808799) *
+                                        0.015,
+                                  ),
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width *
+                                        0.01,
+                                  ),
+                                  Text("قيد المراجعة"),
+                                ],
+                              ),
+                            ),
+                            Tab(
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.brightness_1,
+                                    color: AppColors.orderStatePending,
+                                    size:
+                                        (MediaQuery.of(context).size.height *
+                                            .808799) *
+                                        0.015,
+                                  ),
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width *
+                                        0.01,
+                                  ),
+                                  Text("قيد المعالجة"),
+                                ],
+                              ),
+                            ),
+                            Tab(
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.brightness_1,
+                                    color: AppColors.orderStatePending,
+                                    size:
+                                        (MediaQuery.of(context).size.height *
+                                            .808799) *
+                                        0.015,
+                                  ),
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width *
+                                        0.01,
+                                  ),
+                                  Text("بانتضار التاكيد"),
+                                ],
+                              ),
+                            ),
+                            Tab(
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.brightness_1,
+                                    color: AppColors.orderStateRejected,
+                                    size:
+                                        (MediaQuery.of(context).size.height *
+                                            .808799) *
+                                        0.015,
+                                  ),
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width *
+                                        0.01,
+                                  ),
+                                  Text("مرفوضة"),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+                      : null,
+                ),
+
+                drawer: const SideMenu(),
+                body: IndexedStack(
+                  index: state.currentIndex,
+                  children: [
+                    const OrdersScreen(),
+                    const Center(child: Text('الرئيسية')),
+                    const OrdersMapScreen(),
+                    const ProductsScreen(),
+                    const CommissionScreen(),
+                  ],
+                ),
               ),
             );
           },
